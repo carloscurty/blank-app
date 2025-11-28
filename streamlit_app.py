04d60349148ed192e464f457451a9d1ecf759029
Carlos Curty <68711113+carloscurty@users.noreply.github.com>
2025-11-28 21:48:33 +0000

    teste dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🎈 My new app")
 st.write(
     "🎈 My new Streamlit app"
 )
+
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df

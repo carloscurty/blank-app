7913ca622f5799c4106025337ccf528d9594ee19
Carlos Curty <68711113+carloscurty@users.noreply.github.com>
2025-11-28 21:53:09 +0000

    dataframe 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,8 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "🎈 My new Streamlit app"
-)
-
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))

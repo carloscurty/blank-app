1a9cbe02e3ae7a2ab3c078ca4d42a8e6027b4ce3
Carlos Curty <68711113+carloscurty@users.noreply.github.com>
2025-11-28 22:08:36 +0000

    df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Aqui está - our first attempt at using data to create a table:")
+st.write("Aqui está   a tabela:")
 st.table(pd.DataFrame({
     'first column': [5, 1, 2, 3, 4],
     'second column': [0, 10, 20, 30, 40]

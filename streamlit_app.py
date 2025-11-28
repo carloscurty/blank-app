5d61c5b54dab7fc4a57e8354eae52ee469fa57ba
Carlos Curty <68711113+carloscurty@users.noreply.github.com>
2025-11-28 22:39:22 +0000

    w2

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.line_chart(chart_data)
 #------ Mapa
 
 map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [x, 50] + [37.76, -122.4],
+    np.random.randn(x, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
 st.map(map_data)

fb3dd3960aa38ca37f4e8fba8f0595fcb3fd46f8
Carlos Curty <68711113+carloscurty@users.noreply.github.com>
2025-11-28 22:33:16 +0000

    graf-mapa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,25 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 
-st.write("Aqui está nossa tabela:")
-st.table(pd.DataFrame({
-    'first column': [5, 1, 2, 3, 4],
-    'second column': [0, 10, 20, 30, 40]
-}))
+#dataframe = pd.DataFrame(
+#    np.random.randn(10, 20),
+#    columns=('col %d' % i for i in range(20)))
+
+#st.dataframe(dataframe.style.highlight_max(axis=0))
+
+#------ Gráfico
+chart_data = pd.DataFrame(
+     np.random.randn(50, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+#------ Mapa
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)

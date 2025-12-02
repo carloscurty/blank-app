649e8562255beb5e8cc33781e6b4498e54a16a92
Carlos Curty <68711113+carloscurty@users.noreply.github.com>
2025-12-02 15:28:11 +0000

    df

--- page_3.py
+++ page_3.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import pandas as pd
 
 st.markdown("# Caixa 💰")
 st.sidebar.markdown("# Caixa 💰")
+
+arquivo=st.file_uploader("Upload de arquivo", type=["csv", "xlsx"])
+if arquivo:
+    try:
+        df=pd.read_csv(arquivo)
+        st.success("Arquivo CSV carregado com sucesso!")
+    except Exception as e:
+        try:
+            df=pd.read_excel(arquivo)
+            st.success("Arquivo Excel carregado com sucesso!")
+        except Exception as e:
+            st.error("Erro ao carregar o arquivo. Por favor, envie um arquivo CSV ou Excel válido.")
+
+    #st.dataframe(df)
+
+#print(df.describe())
